06c271b767c2c87cc719ced81d45917c84554cc5
vineeth <bala.saivineeth2020@vitstudent.ac.in>
2023-03-04 23:56:24 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Card from './Card';//importing
+import FormField from './FormField';
+import Loader from './Loader';
+
+export {//export
+  Card,
+  FormField,
+  Loader,
+};
---
+++ client/src/page/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import CreatePost from './CreatePost';
+
+export {//export in one object
+  Home,
+  CreatePost,
+};
